9c8b37c9f378105d6210688e6f00c7d15625b4ca
Snubb <60389050+Snubb@users.noreply.github.com>
2021-03-16 11:09:31 +0100

    Big tree och ansikte

--- src/Test_canv.java
+++ src/Test_canv.java
@@ -1,3 +1,4 @@
+import javax.crypto.Cipher;
 import javax.swing.*;
 import java.awt.*;
 
@@ -25,7 +26,43 @@ public class Test_canv extends Canvas {
         drawTree(g,150,195);
         drawTree(g,160,200);
 
+        drawBigTree(g,300,300);
 
+        drawMan(g,400,300);
+
+
+    }
+
+    private void drawMan(Graphics g, int x, int y) {
+        //Face
+        g.setColor(new Color(218, 189, 171, 255));
+        g.fillOval(0+x,0+y,100,100);
+        //Eyes
+        g.setColor(new Color(255,255,255));
+        g.fillOval(20+x,30+y,15,15);
+        g.fillOval(65+x,30+y,15,15);
+        //Pupils
+        g.setColor(Color.black);
+        g.fillOval(25+x,35+y,7,7);
+        g.fillOval(70+x,35+y,7,7);
+        //Hair
+        g.setColor(new Color(0,0,0));
+        g.fillArc(0+x,0+y,100,70,360,180);
+        //Mouth
+        g.setColor(Color.black);
+        g.fillRect(30+x,70+y,40,5);
+
+
+    }
+
+    private void drawBigTree(Graphics g, int x, int y) {
+        g.setColor(new Color(141, 71, 47));
+        g.fillRect(40+x,30+y,20,70);
+
+        g.setColor(new Color(54, 127, 38));
+        g.fillOval(10+x,20+y,40,40);
+        g.fillOval(50+x,20+y,40,40);
+        g.fillOval(30+x,0+y,40,40);
     }
 
     private void drawTree(Graphics g, int x, int y) {
